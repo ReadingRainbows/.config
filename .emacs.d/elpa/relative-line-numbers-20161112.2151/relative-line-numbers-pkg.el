74efe3e7aa06d84eb61523f87300d676e5c1bf85
Jérôme Martel <zinzon@Zinzon>
2017-06-10 20:27:33 -0400

    Added latest change to emacs config. Support helm,evil leader, session load, etc.

---
+++ .emacs.d/elpa/relative-line-numbers-20161112.2151/relative-line-numbers-pkg.el
@@ -0,0 +1,1 @@
+(define-package "relative-line-numbers" "20161112.2151" "Display relative line numbers on the margin" '((emacs "24")) :commit "38b5f9065aec008d9ad94fe5597338463aa1aa63" :url "https://github.com/Fanael/relative-line-numbers")
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,67 @@
+;;tell where the program ctags is
+(setq path-to-ctags "/usr/bin/ctags")
+;; tell where to store desktop save
+(defvar emacs-configuration-directory
+      "~/.emacs.d/"
+          "The directory where the emacs configuration files are stored.")
+;;tell how to name the file containing the elscreen save
+(defvar elscreen-tab-configuration-store-filename
+      (concat emacs-configuration-directory ".elscreen")
+          "The file where the elscreen tab configuration is stored.")
+;;Custom function to kill all except the current window.
+(defun kill-other-buffers ()
+        "Kill all other buffers."
+        (interactive)
+        (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+;;Create a function to generate ctags
+(defun create-tags (dir-name)
+      "Create tags file."
+      (interactive "DDirectory: ")
+      (shell-command
+      (format "%s -f TAGS -e -R %s" path-to-ctags (directory-file-name dir-name)))
+      )
+;;Save the current buffer layout
+(defun session-save ()
+      "Store the elscreen tab configuration."
+      (interactive)
+      (if (desktop-save emacs-configuration-directory)
+          (with-temp-file elscreen-tab-configuration-store-filename
+            (insert (prin1-to-string (elscreen-get-screen-to-name-alist))))))
+
+;; Load session including tabs
+(defun session-load ()
+      "Restore the elscreen tab configuration."
+      (interactive)
+      (if (desktop-read)
+        (let ((screens (reverse
+                       (read
+                        (with-temp-buffer
+                        (insert-file-contents elscreen-tab-configuration-store-filename)
+                        (buffer-string))))))
+        (while screens
+        (setq screen (car (car screens)))
+        (setq buffers (split-string (cdr (car screens)) ":"))
+        (if (eq screen 0)
+          (switch-to-buffer (car buffers))
+          (elscreen-find-and-goto-by-buffer (car buffers) t t))
+        (while (cdr buffers)
+          (switch-to-buffer-other-window (car (cdr buffers)))
+          (setq buffers (cdr buffers)))
+        (setq screens (cdr screens))))))
+;;save the current working window and path
+(desktop-save-mode 1)
+;;Set C syntax check to stroustrup with 4 as a basic offset
+(setq c-default-style "stroustrup"
+      c-basic-offset 4)
+(setq comment-style 'extraline)
+
+;;relative-line-numbers
+(add-hook 'prog-mode-hook 'relative-line-numbers-mode t)
+(add-hook 'prog-mode-hook 'line-number-mode t)
+(add-hook 'prog-mode-hook 'column-number-mode t)
+
+;;Evil doesnt come with ctrl+u as default so we get it now.
+(setq evil-want-C-u-scroll t)
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
                         ("org" . "http://orgmode.org/elpa/")
                         ("gnu" . "http://elpa.gnu.org/packages/")))
@@ -5,8 +69,10 @@
 (package-initialize)
 
 (load-theme 'suscolors t)
+;;elscreen to support evil tabs
 (load "elscreen")
 (elscreen-start)
+;;Add neotree to browse to file
 (add-to-list 'load-path "~/Documents/Emacs/neotree")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
@@ -17,15 +83,20 @@
                                         (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
                                         (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
                                         (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
-
+;;set powerline for evil
 (require 'powerline)
 (powerline-center-evil-theme)
 
+;;gdb graphical mode
+(setq gdb-many-windows t)
+(require 'org)
+;;Use evil-magit
 (require 'evil-magit)
 ;; Tell emacs where is your personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; load the packaged named xyz.
+;;load evil-tabs (dependency on elscreen) to have vim tabs.
 (load "evil-tabs") ;; best not to include the ending “.el” or “.elc”
 (global-evil-tabs-mode t)
 (custom-set-variables
@@ -43,9 +114,76 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
+;;Remove splash screen at startup
 (setq inhibit-splash-screen t)
-(require 'org)
-(setq evil-want-C-u-scroll t)
+;;Use vim leader for extra shortcut
+(global-evil-leader-mode)
+;;define new leader function
+(evil-leader/set-key
+    "w" 'find-file-other-window
+    "f" 'find-file-other-frame
+    "q" 'kill-buffer-and-window
+    "x" 'kill-other-buffers
+    "k" 'elscreen-next
+    "j" 'elscreen-previous
+    "c" 'elscreen-create 
+    "e" 'find-file
+    "n" 'other-window
+    "<c" 'create-tags
+    "<t" 'find-tag
+    "<p" 'pop-tag-mark
+    "gb" 'gud-break
+    "gs" 'gud-step
+    "gn" 'gud-next
+    "gc" 'gud-cont
+    "gr" 'gud-remove
+    "hs" 'helm-semantic-or-imenu
+    "ss" 'session-save
+    "sl" 'session-load)
+;;gh	outline-up-heading
+;;gj	org-forward-heading-same-level
+;;gk	org-backward-heading-same-level
+;;gl	outline-next-visible-heading
+;;t	org-todo
+;;T	org-insert-todo-heading nil
+;;H	org-shiftleft
+;;J	org-shiftdown
+;;K	org-shiftup
+;;L	org-shiftright
+;;o	always-insert-item
+;;O	org-insert-heading
+;;’$’	org-end-of-line
+;;’^’	org-beginning-o-line
+;;<	org-metaleft
+;;>	org-metaright
+;;<leader>a	org-agenda
+;;<leader>t	org-show-todo-tree
+;;<leader>c	org-archive-subtree
+;;<leader>l	evil-org-open-links
+;;<leader>o	evil-org-recompute-clocks
+;;TAB	org-cycle
+;;M-l	org-metaright
+;;M-h	org-metaleft
+;;M-k	org-metaup
+;;M-j	org-metadown
+;;M-L	org-shiftmetaright
+;;M-H	org-shiftmetaleft
+;;M-K	org-shiftmetaup
+;;M-J	org-shiftmetadown
+;;M-o	org-insert-heading+org-metaright
+;;M-t	org-insert-todo-heading nil+ org-metaright
+(evil-leader/set-leader "<SPC>")
+(setq evil-leader/in-all-states 1)
+;; Set up evil-org
+;; Set up helm
+(require 'helm-config)
+(helm-mode 1)
+
 (require 'evil)
+;;Set the new pop up frame as evil-motion mode (we can use hjkl)
+(setq evil-motion-state-modes (append evil-emacs-state-modes evil-motion-state-modes))
+(setq evil-emacs-state-modes nil)
+;;Prevent cursor from going back when leaving insert mode
+(setq evil-move-cursor-back nil)
 (evil-mode 1)
+(semantic-mode 1)
